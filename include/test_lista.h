01f0f66a0c492bdf353c58ae784b6348718f4055
Stefano <Stefa.ro04@hotmail.com>
2026-05-20 22:38:11 +0200

    Finiti di scrivere le funzioni della lista d'attesa e completati i test

---
+++ include/test_lista.h
@@ -0,0 +1,5 @@
+void test_inizializza();
+void test_inserimento();
+void test_visualizzazione();
+void test_chiudi_turno();
+void test_cancella_prenotazione();
--- lista-attesa.h
+++
@@ -1,6 +0,0 @@
-typedef struct lista *lista;
-
-lista *elim_preontazione(lista *, int);
-lista *chiudi_turno(lista *, int);
-lista *inizializza_lista();
-void cancella_lista(lista);
